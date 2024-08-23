4674e8e0e3e1ee09570000761f98b1123c1db08c
JP1901 <dj.jp81@gmail.com>
2024-08-23 14:26:13 -0500

    Adding extra button to the project

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,10 +1,10 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-    { id: 4, name: 'Jefferson Farfan', age: 26, department: 'IT', salary: 45000 },
-    { id: 5, name: 'Paolo Guerrero', age: 23, department: 'Industry', salary: 15000 },
-    { id: 6, name: 'George Forsyth', age: 40, department: 'HR', salary: 80000 }
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'Javascript'},
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:'Python'},
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization:'C#' },
+    { id: 4, name: 'Jefferson Farfan', age: 26, department: 'IT', salary: 45000, specialization:'Javascript'},
+    { id: 5, name: 'Paolo Guerrero', age: 23, department: 'Industry', salary: 15000, specialization:'Javascript'},
+    { id: 6, name: 'George Forsyth', age: 40, department: 'HR', salary: 80000,specialization:'Python'}
     //... More employee records can be added here
   ];
 
@@ -34,4 +34,20 @@ function findEmployeeById(employeeId) {
     else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
     }
+}
+
+function filterBySpecializationJs(){
+  const jsSpec = employees.filter(employe => employe.specialization === 'Javascript');
+  const jsSpecDisplay = jsSpec.map((employee,index) => `<p>${employee.id}: ${employee.name} - ${employee.department} - ${employee.specialization}</p>`).join('');
+  document.getElementById('employeesDetails').innerHTML = jsSpecDisplay;
+
+    // const foundEmpEsp = employees.find(employee => employee.specialization === employeeEsp);
+    // if(foundEmpEsp){
+    //   document.getElementById('employeesDetails').innerHTML = `<p>${foundEmpEsp.id}: ${foundEmpEsp.name} - ${foundEmpEsp.department} - ${foundEmpEsp.specialization}</p>`;
+    // } else {
+    //   document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+    
+    // }
+
+
 }
